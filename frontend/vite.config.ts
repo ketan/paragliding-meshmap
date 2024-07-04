7171b32a3c1b006b3c68dc3f087cfac69f5ced8c
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2024-07-04 15:26:41 +0530

    Remove unwanted deps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,27 +2,28 @@ import preact from '@preact/preset-vite'
 import path, { resolve } from 'path'
 import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
+import Inspect from 'vite-plugin-inspect'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), Inspect()],
+
   root: __dirname,
-  // publicDir: `${__dirname}/public`,
   build: {
     sourcemap: true,
     assetsInlineLimit: 1024 * 1024, // 1024KiB
     emptyOutDir: true,
     rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            return 'vendor'
-          }
-        },
-      },
+      // output: {
+      //   manualChunks: (id) => {
+      //     if (id.includes('node_modules')) {
+      //       return 'vendor'
+      //     }
+      //   },
+      // },
       input: {
         index: resolve(__dirname, `index.html`),
       },

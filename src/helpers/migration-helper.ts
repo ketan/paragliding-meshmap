79e87c5ee400c070d84f56f064d5adee47561a4b
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2024-07-06 11:38:35 +0530

    Ad helper functions

--- src/helpers/migration-helper.ts
+++ src/helpers/migration-helper.ts
@@ -1,4 +1,5 @@
 import { QueryRunner, TableIndex } from 'typeorm'
+import { AppDataSource } from '../data-source.js'
 
 export function primaryKeyType(queryRunner: QueryRunner) {
   if (queryRunner.connection.options.type.includes('sqlite')) {
@@ -20,4 +21,24 @@ export async function dropIndices(queryRunner: QueryRunner, tableName: string, c
     tableName,
     columnNames.map((eachColumn) => new TableIndex({ name: `${tableName}_${eachColumn}_idx`, columnNames: [eachColumn] }))
   )
+}
+
+export function dateTimeType() {
+  if (AppDataSource.driver.supportedDataTypes.includes('datetime')) {
+    return 'datetime'
+  } else if (AppDataSource.driver.supportedDataTypes.includes('timestamp')) {
+    return 'timestamp'
+  }
+  throw new Error(
+    `Unsupported datetime type for ${AppDataSource.options.type}. Supported types are ${AppDataSource.driver.supportedDataTypes}`
+  )
+}
+
+export function blobType() {
+  if (AppDataSource.driver.supportedDataTypes.includes('blob')) {
+    return 'blob'
+  } else if (AppDataSource.driver.supportedDataTypes.includes('bytea')) {
+    return 'bytea'
+  }
+  throw new Error(`Unsupported blob type for ${AppDataSource.options.type}. Supported types are ${AppDataSource.driver.supportedDataTypes}`)
 }

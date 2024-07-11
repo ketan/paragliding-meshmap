25e2a74831fb10005278f62b504ca83f2b014dc9
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2024-07-11 12:22:37 +0530

    Clear the search box after node is selected in search results

--- frontend/src/searchbarapp.tsx
+++ frontend/src/searchbarapp.tsx
@@ -79,6 +79,7 @@ export class SearchBarApp extends PureComponent<AllData & { selectCallback: (nod
         <input
           type="text"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          value={this.state.searchText}
           onInput={this.applyFilter.bind(this)}
           placeholder={`Search ${this.props.newerNodesWithPosition.length} nodes...`}
         />

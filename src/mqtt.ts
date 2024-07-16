a9cd1017155a570a0e06ad9bed12e97365f8484a
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2024-07-16 23:29:08 +0530

    Initialize datasource when running standalone MQTT

--- src/mqtt.ts
+++ src/mqtt.ts
@@ -2,9 +2,13 @@
 import 'dotenv/config'
 
 //
+import { AppDataSource } from '#config/data-source'
 import { mqttProcessor } from '#mqtt/main'
 import { MQTTCLIOptions, mqttCLIParse } from './helpers/cli.js'
 
 const cliOptions: MQTTCLIOptions = mqttCLIParse()
+
+await AppDataSource.initialize()
+await AppDataSource.runMigrations({ transaction: 'each' })
 
 mqttProcessor(cliOptions)

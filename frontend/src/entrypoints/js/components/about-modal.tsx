698e47e700bf93894aa3755e9db2f00c1773f960
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2024-08-27 21:39:21 +0530

    Improve about modal

--- frontend/src/entrypoints/js/components/about-modal.tsx
+++ frontend/src/entrypoints/js/components/about-modal.tsx
@@ -1,9 +1,10 @@
 import icon from '../../../assets/images/icon.png'
 import { GithubIcon } from '../utils/icon-constants'
 import { Modal, ModalBaseProps } from './modal'
+import { isMobile } from '../utils/ui-util.tsx'
 
 const aboutModalTitle = (
-  <div className="w-min-full mx-auto text-center p-2 pt-4 pb-4 text-sm md:text-md">
+  <div className="w-min-full mx-auto text-center pt-4 text-sm md:text-md">
     <div className="relative mx-auto w-16 h-16 mb-1">
       <div className="text-4xl absolute -top-2.5 -right-1.5">&#x1FA82;</div>
       <img className="w-16 h-16 rounded bg-opacity-90" src={icon} alt="Meshtastic icon" />
@@ -34,6 +35,14 @@ const aboutModalTitle = (
   </div>
 )
 
+function meshtasticIndiaTelegramLink() {
+  if (isMobile()) {
+    return 'https://t.me/meshtastic_india'
+  } else {
+    return 'https://web.telegram.org/k/#@meshtastic_india'
+  }
+}
+
 export function AboutModal({ onClose, isOpen }: ModalBaseProps) {
   return (
     <Modal isOpen={isOpen} onClose={onClose} header={`About`} footerButtons={[]}>
@@ -41,57 +50,62 @@ export function AboutModal({ onClose, isOpen }: ModalBaseProps) {
       {/* app info */}
       <div className="pt-4 pb-4 text-sm md:text-md">
         {/* faq */}
-        <div>
-          <h3 className="font-semibold my-2">FAQ</h3>
-          {/*<h3 className="font-bold mb-2">FAQ</h3>*/}
-          <div className="space-y-2">
+        <h3 className="font-semibold my-2">FAQ</h3>
+        {/*<h3 className="font-bold mb-2">FAQ</h3>*/}
+        <div className="space-y-2">
+          <div className="bg-gray-100 rounded p-2 border border-gray-200">
+            <div className="font-semibold">I found an issue with this site, what do I do?</div>
+            <div>
+              Report it on the{' '}
+              <a target="_blank" rel="noreferrer" href={meshtasticIndiaTelegramLink()}>
+                telegram channel.
+              </a>
+            </div>
+          </div>
+          <div className="bg-gray-100 rounded p-2 border border-gray-200">
+            <div className="font-semibold">How do I see myself on the map?</div>
+            <div>
+              Check{' '}
+              <a target="_blank" rel="noreferrer" href="https://bircom.in">
+                this page
+              </a>{' '}
+              on getting started.
+            </div>
+          </div>
+          {
             <div className="bg-gray-100 rounded p-2 border border-gray-200">
-              <div className="font-semibold">I found a bug, what do I do?</div>
+              <div className="font-semibold">Concerned about privacy?</div>
               <div>
-                <a target="_blank" rel="noreferrer" href="https://github.com/ketan/paragliding-meshmap">
-                  Open an issue
-                </a>{' '}
-                on GitHub.
+                This Meshtastic tracker is mainly intended for search-and-rescue (SAR) operations and offline messaging for retrieve etc.
+                Consequently, your name, location, and any messages you send will be visible to everyone. Please avoid sharing any personal
+                information on the Meshtastic service that you wouldn&apos;t want others to see.
               </div>
             </div>
-            <div className="bg-gray-100 rounded p-2 border border-gray-200">
-              <div className="font-semibold">How do I add my node to the map?</div>
-              <div>Your node, or a node that hears your node must uplink to MQTT.</div>
-              <div>Your position packet must be unencrypted, or encrypted with the default key.</div>
-            </div>
-            <div className="bg-gray-100 rounded p-2 border border-gray-200">
-              <div className="font-semibold">How do I remove myself from the map?</div>
-              <div>Meshtastic devices that have not been heard for 7 days are automatically removed.</div>
-              <div>Disable position reporting in your node to prevent it coming back.</div>
-              <div>Use custom encryption keys so the public cannot see your position data.</div>
-            </div>
-          </div>
+          }
         </div>
         {/* legal */}
-        <div>
-          <h3 className="font-semibold my-2">Legal</h3>
-          <div className="bg-gray-100 rounded p-2 border border-gray-200">
-            <ul className="list-disc list-inside">
-              <li>
-                This project is not affiliated with or endorsed by the{' '}
-                <a target="_blank" rel="noreferrer" href="https://meshtastic.org">
-                  Meshtastic
-                </a>{' '}
-                project.
-              </li>
-              <li>The Meshtastic logo is the trademark of Meshtastic LLC.</li>
-              <li>
-                Map tiles provided by{' '}
-                <a target="_blank" rel="noreferrer" href="https://www.openstreetmap.org/copyright">
-                  OpenStreetMap
-                </a>{' '}
-                and{' '}
-                <a target="_blank" rel="noreferrer" href="https://maps.google.com">
-                  Google
-                </a>
-              </li>
-            </ul>
-          </div>
+        <h3 className="font-semibold my-2">Legal</h3>
+        <div className="bg-gray-100 rounded p-2 border border-gray-200">
+          <ul className="list-disc list-inside">
+            <li>
+              This project is not affiliated with or endorsed by the{' '}
+              <a target="_blank" rel="noreferrer" href="https://meshtastic.org">
+                Meshtastic
+              </a>{' '}
+              project.
+            </li>
+            <li>The Meshtastic logo is the trademark of Meshtastic LLC.</li>
+            <li>
+              Map tiles provided by{' '}
+              <a target="_blank" rel="noreferrer" href="https://www.openstreetmap.org/copyright">
+                OpenStreetMap
+              </a>{' '}
+              and{' '}
+              <a target="_blank" rel="noreferrer" href="https://maps.google.com">
+                Google
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </Modal>

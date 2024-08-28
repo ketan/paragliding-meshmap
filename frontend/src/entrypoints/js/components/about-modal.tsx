cbf5bdb1c6547c3c3d9c1183c08a9379616f7849
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2024-08-28 22:46:25 +0530

    Refactor the about modal

--- frontend/src/entrypoints/js/components/about-modal.tsx
+++ frontend/src/entrypoints/js/components/about-modal.tsx
@@ -37,6 +37,78 @@ const aboutModalTitle = (
   </div>
 )
 
+function foundABug() {
+  return (
+    <div className="bg-gray-100 rounded p-2 border border-gray-200">
+      <div className="font-semibold">I found an issue with this site, what do I do?</div>
+      <div>
+        Report it on the{' '}
+        <a target="_blank" rel="noreferrer" href={meshtasticIndiaTelegramLink()}>
+          telegram channel.
+        </a>
+      </div>
+    </div>
+  )
+}
+
+function howDoIFindMyselfOnTheMap() {
+  return (
+    <div className="bg-gray-100 rounded p-2 border border-gray-200">
+      <div className="font-semibold">How do I see myself on the map?</div>
+      <div>
+        Check{' '}
+        <a target="_blank" rel="noreferrer" href="https://bircom.in">
+          this page
+        </a>{' '}
+        on getting started.
+      </div>
+    </div>
+  )
+}
+
+function privacy() {
+  return (
+    <div className="bg-gray-100 rounded p-2 border border-gray-200">
+      <div className="font-semibold">Concerned about privacy?</div>
+      <div>
+        This Meshtastic tracker is mainly intended for search-and-rescue (SAR) operations and offline messaging for retrieve etc.
+        Consequently, your name, location, and any messages you send will be visible to everyone. Please avoid sharing any personal
+        information on the Meshtastic service that you wouldn&apos;t want others to see.
+      </div>
+    </div>
+  )
+}
+
+function legal() {
+  return (
+    <>
+      <h3 className="font-semibold my-2">Legal</h3>
+      <div className="bg-gray-100 rounded p-2 border border-gray-200">
+        <ul className="list-disc list-inside">
+          <li>
+            This project is not affiliated with or endorsed by the{' '}
+            <a target="_blank" rel="noreferrer" href="https://meshtastic.org">
+              Meshtastic
+            </a>{' '}
+            project.
+          </li>
+          <li>The Meshtastic logo is the trademark of Meshtastic LLC.</li>
+          <li>
+            Map tiles provided by{' '}
+            <a target="_blank" rel="noreferrer" href="https://www.openstreetmap.org/copyright">
+              OpenStreetMap
+            </a>{' '}
+            and{' '}
+            <a target="_blank" rel="noreferrer" href="https://maps.google.com">
+              Google
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
 export function AboutModal({ onClose, isOpen }: ModalBaseProps) {
   return (
     <Modal isOpen={isOpen} onClose={onClose} header={`About`} footerButtons={[]}>
@@ -45,62 +117,13 @@ export function AboutModal({ onClose, isOpen }: ModalBaseProps) {
       <div className="pt-4 pb-4 text-sm md:text-md">
         {/* faq */}
         <h3 className="font-semibold my-2">FAQ</h3>
-        {/*<h3 className="font-bold mb-2">FAQ</h3>*/}
         <div className="space-y-2">
-          <div className="bg-gray-100 rounded p-2 border border-gray-200">
-            <div className="font-semibold">I found an issue with this site, what do I do?</div>
-            <div>
-              Report it on the{' '}
-              <a target="_blank" rel="noreferrer" href={meshtasticIndiaTelegramLink()}>
-                telegram channel.
-              </a>
-            </div>
-          </div>
-          <div className="bg-gray-100 rounded p-2 border border-gray-200">
-            <div className="font-semibold">How do I see myself on the map?</div>
-            <div>
-              Check{' '}
-              <a target="_blank" rel="noreferrer" href="https://bircom.in">
-                this page
-              </a>{' '}
-              on getting started.
-            </div>
-          </div>
-          {
-            <div className="bg-gray-100 rounded p-2 border border-gray-200">
-              <div className="font-semibold">Concerned about privacy?</div>
-              <div>
-                This Meshtastic tracker is mainly intended for search-and-rescue (SAR) operations and offline messaging for retrieve etc.
-                Consequently, your name, location, and any messages you send will be visible to everyone. Please avoid sharing any personal
-                information on the Meshtastic service that you wouldn&apos;t want others to see.
-              </div>
-            </div>
-          }
+          {foundABug()}
+          {howDoIFindMyselfOnTheMap()}
+          {privacy()}
         </div>
         {/* legal */}
-        <h3 className="font-semibold my-2">Legal</h3>
-        <div className="bg-gray-100 rounded p-2 border border-gray-200">
-          <ul className="list-disc list-inside">
-            <li>
-              This project is not affiliated with or endorsed by the{' '}
-              <a target="_blank" rel="noreferrer" href="https://meshtastic.org">
-                Meshtastic
-              </a>{' '}
-              project.
-            </li>
-            <li>The Meshtastic logo is the trademark of Meshtastic LLC.</li>
-            <li>
-              Map tiles provided by{' '}
-              <a target="_blank" rel="noreferrer" href="https://www.openstreetmap.org/copyright">
-                OpenStreetMap
-              </a>{' '}
-              and{' '}
-              <a target="_blank" rel="noreferrer" href="https://maps.google.com">
-                Google
-              </a>
-            </li>
-          </ul>
-        </div>
+        {legal()}
       </div>
     </Modal>
   )

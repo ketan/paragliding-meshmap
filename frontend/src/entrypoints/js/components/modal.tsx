46b45d2244250ba21edc9474d7d14d8a4a265ea0
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2024-08-31 01:14:32 +0530

    Ensure that the modals are vertically centered

--- frontend/src/entrypoints/js/components/modal.tsx
+++ frontend/src/entrypoints/js/components/modal.tsx
@@ -90,9 +90,9 @@ export function Modal({ isOpen, onClose, header, footerButtons, headerButtons, c
   })
 
   return (
-    <div className="fixed inset-0 z-[1000] flex items-center justify-center bg-gray-900 bg-opacity-75 w-[100vw] h-[100vh]">
+    <div className="fixed inset-0 z-[1000] flex items-center justify-center bg-gray-900 bg-opacity-75 w-full h-full">
       <div
-        className="relative w-[95%] max-w-[95%] sm:w-[90%] sm:max-w-[90%] md:max-w-2xl lg:max-w-2xl mx-auto bg-white rounded-lg shadow-lg"
+        className="relative w-[95%] max-w-[95%] sm:w-[90%] sm:max-w-[90%] md:max-w-2xl lg:max-w-2xl mx-auto bg-white rounded-lg shadow-lg max-h-[90vh] overflow-hidden"
         ref={ref}
       >
         <div className="flex justify-between items-center p-2 border-b shadow-md">
@@ -107,7 +107,7 @@ export function Modal({ isOpen, onClose, header, footerButtons, headerButtons, c
             })}
           </div>
         </div>
-        <div className="p-2 overflow-y-auto max-h-[80vh]">{children}</div>
+        <div className="p-2 overflow-y-auto max-h-[70vh]">{children}</div>
         {bottomButtons()}
       </div>
     </div>

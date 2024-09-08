18a333e9ca8378ed3d3d30d85335eb2cb1972b2d
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2024-09-08 23:36:28 +0530

    Don't run tests in parallel. Increase timeout

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -7,4 +7,6 @@ module.exports = {
   spec: 'test/**/*.spec.ts',
   require: ['ts-node/register', './test/hooks.ts'],
   'node-option': ['experimental-specifier-resolution=node', 'loader=ts-node/esm'],
+  timeout: 5000,
+  jobs: 1,
 }

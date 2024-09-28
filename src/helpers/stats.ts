1ae87777cea67c1f03c36d6f9e557c6aa1250472
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2024-09-28 13:55:21 +0530

    Add more stats

--- src/helpers/stats.ts
+++ src/helpers/stats.ts
@@ -1,6 +1,8 @@
 import { DataSource } from 'typeorm'
 import debug from 'debug'
 import Position from '#entity/position'
+import DeviceMetric from '#entity/device_metric'
+import EnvironmentMetric from '#entity/environment_metric'
 
 async function dumpCounts(db: DataSource, logger: debug.Debugger) {
   const response = await db.query<
@@ -31,7 +33,43 @@ async function dumpPositionStats(db: DataSource, logger: debug.Debugger) {
   logger(`Position count grouped by node`, response)
 }
 
+async function dumpDeviceMetricsStats(db: DataSource, logger: debug.Debugger) {
+  const deviceMetricsRepository = db.getRepository(DeviceMetric)
+  const twentyFourHoursAgo = new Date(Date.now() - 24 * 60 * 60 * 1000)
+
+  const response = await deviceMetricsRepository
+    .createQueryBuilder('deviceMetrics')
+    .select('deviceMetrics.nodeId', 'node_id')
+    .addSelect('COUNT(*)', 'metrics_count')
+    .where('deviceMetrics.createdAt >= :twentyFourHoursAgo', { twentyFourHoursAgo })
+    .groupBy('deviceMetrics.nodeId')
+    .orderBy('metrics_count', 'DESC')
+    .limit(20)
+    .getRawMany()
+
+  logger(`Device metrics count grouped by node`, response)
+}
+
+async function dumpEnvironmentMetricsStats(db: DataSource, logger: debug.Debugger) {
+  const environmentMetricsRepository = db.getRepository(EnvironmentMetric)
+  const twentyFourHoursAgo = new Date(Date.now() - 24 * 60 * 60 * 1000)
+
+  const response = await environmentMetricsRepository
+    .createQueryBuilder('environmentMetrics')
+    .select('environmentMetrics.nodeId', 'node_id')
+    .addSelect('COUNT(*)', 'metrics_count')
+    .where('environmentMetrics.createdAt >= :twentyFourHoursAgo', { twentyFourHoursAgo })
+    .groupBy('environmentMetrics.nodeId')
+    .orderBy('metrics_count', 'DESC')
+    .limit(20)
+    .getRawMany()
+
+  logger(`Environment metrics count grouped by node`, response)
+}
+
 export async function dumpStats(db: DataSource, logger: debug.Debugger) {
   await dumpCounts(db, logger)
   await dumpPositionStats(db, logger)
+  await dumpDeviceMetricsStats(db, logger)
+  await dumpEnvironmentMetricsStats(db, logger)
 }

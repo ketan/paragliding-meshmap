8a59ce3c286eabe3bd43cec9a605610202f949fb
Stef Walter <stefw@redhat.com>
2024-10-08 06:46:33 +0530

    WIP: Show activity in the UI

--- frontend/src/db-entities.d.ts
+++ frontend/src/db-entities.d.ts
@@ -91,6 +91,7 @@ export interface NodesEntity {
   updatedAt: string
   uptimeSeconds?: number
   voltage?: number
+  activity?: string | null
 }
 export interface PositionsEntity {
   altitude?: number

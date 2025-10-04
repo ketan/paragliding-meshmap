eb078d89d468616edc954718337fc6264683aefc
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2025-10-04 22:46:11 +0530

    fix: remove a debug statement

--- frontend/src/entrypoints/js/utils/fetch-patch.ts
+++ frontend/src/entrypoints/js/utils/fetch-patch.ts
@@ -29,7 +29,6 @@ window.fetch = async (input: RequestInfo | URL, init?: RequestInit): Promise<Res
 
   const serverVersion = response.headers.get('X-App-Version')
   if (serverVersion && serverVersion !== currentVersion) {
-    debugger
     window.location.reload()
   }
 
